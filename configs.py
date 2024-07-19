baf74bd8bdd82661035cfb3dc624bc7635627c88
Devensh22345 <kumardevensh0@gmail.com>
2024-07-19 13:27:26 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,8 +9,8 @@ class Config:
     API_ID = int(getenv("API_ID", "22207976"))
     API_HASH = getenv("API_HASH", "5c0ad7c48a86afac87630ba28b42560d")
     BOT_TOKEN = getenv("BOT_TOKEN", "7365852964:AAFsKlal2wRxWjZPxYQ4sOPaz7KJyqJaIWQ")
-    FSUB = getenv("FSUB", "VJ_Botz")
-    CHID = int(getenv("CHID", "-1001623633000"))
+    FSUB = getenv("FSUB")
+    CHID = int(getenv("CHID"))
     SUDO = list(map(int, getenv("SUDO", "6872968794").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Autofilterbot:Autofilterbot@cluster0.1oipdqu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
