be904cdc06ef92856280e7a86ad076a5a0a6d7e2
Devensh22345 <kumardevensh0@gmail.com>
2024-07-20 06:17:38 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,8 +9,8 @@ class Config:
     API_ID = int(getenv("API_ID", "22207976"))
     API_HASH = getenv("API_HASH", "5c0ad7c48a86afac87630ba28b42560d")
     BOT_TOKEN = getenv("BOT_TOKEN", "7365852964:AAFsKlal2wRxWjZPxYQ4sOPaz7KJyqJaIWQ")
-    FSUB = getenv("FSUB", "Dk_animes")
-    CHID = int(getenv("CHID", "-1002154311129"))
+    FSUB = getenv("FSUB", "Dk_anime_Group")
+    CHID = int(getenv("CHID", "-1002204196114"))
     SUDO = list(map(int, getenv("SUDO", "6872968794").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Autofilterbot:Autofilterbot@cluster0.1oipdqu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
