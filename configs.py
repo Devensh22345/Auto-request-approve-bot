067eb6b5a60edff49f1c4c0cbe6e8a7d56b29fd5
Devensh22345 <kumardevensh0@gmail.com>
2025-01-23 21:18:29 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,6 +1,3 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
 
 
 from os import path, getenv
@@ -8,12 +5,9 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "22207976"))
     API_HASH = getenv("API_HASH", "5c0ad7c48a86afac87630ba28b42560d")
-    BOT_TOKEN = getenv("BOT_TOKEN", "7365852964:AAFsKlal2wRxWjZPxYQ4sOPaz7KJyqJaIWQ")
-    SUDO = list(map(int, getenv("SUDO", "6872968794").split()))
+    BOT_TOKEN = getenv("BOT_TOKEN", "7200841820:AAFgKy1xg8EYeIMyvcibSmf7g1vinhW1VNQ")
+    SUDO = list(map(int, getenv("SUDO", "6872968794")))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Autofilterbot:Autofilterbot@cluster0.1oipdqu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
 cfg = Config()
 
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
